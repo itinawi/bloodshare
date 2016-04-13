1284c3ac536649df8668bb435ce73efdd36d7310
Ihssan <itinawi@mit.edu>
2016-04-13 18:06:50 -0400

    added the choose mode page

---
+++ js/mode_select.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  $('.donor_mode').click(function(e) {
+    window.open("http://www.w3schools.com");
+  });
+
+  $('.seeker_mode').click(function(e) {
+    window.open("http://www.w3schools.com");
+  });
+
+});
