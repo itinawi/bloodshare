4b78603fc72c37ef02ce053bac6f9c12807c6eef
Usman Ayyaz <uayyaz@mit.edu>
2016-04-13 22:11:40 -0400

    23432422424

--- js/seeker.js
+++ js/seeker.js
@@ -1,28 +1,6 @@
 $(document).ready(function() {
 
-$('#slider1').slider({
-    value:1,
-    min: 1,
-    max: 10,
-    step: 1,
-    slide: function( event, ui ) {
-      $('#emergency_level').text(ui.value);
-    }
-  });
-
-$('#slider2').slider({
-    value:1,
-    min: 1,
-    max: 10,
-    step: 1,
-    slide: function( event, ui ) {
-      $('#donor_proximity').text(ui.value + 'km');
-    }
-  });
-
-$(document).ready(function() {
-
-  $('.submit').click(function(e){
+  $('#submit').click(function(e){
     window.location.replace("http://www.mit.edu/~uayyaz/bloodshare/GR4/results.html");
   })
 
