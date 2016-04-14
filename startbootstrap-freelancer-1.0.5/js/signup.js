2077970132febbd36b42192dcf75408bf0049fc0
OmerCerrahoglu <omer_cerrahoglu2000@yahoo.com>
2016-04-13 21:42:18 -0400

    more signup to be able to pull

--- startbootstrap-freelancer-1.0.5/js/signup.js
+++ startbootstrap-freelancer-1.0.5/js/signup.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 	
-	$("#lg_birthday").datepicker({ changeYear: true, changeMonth: true});
+	$("#lg_birthday").datepicker({ 
+		changeYear: true, 
+		changeMonth: true, 
+		yearRange: '1900:2013',
+		dateFormat: 'mm-dd-yy',
+	    defaultDate: '01-01-1995'});
 
 });
