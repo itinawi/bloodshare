7b648f89c8adbb751dd72eccb33e419576b14f41
Usman Ayyaz <uayyaz@mit.edu>
2016-05-02 11:26:45 -0400

    adding

--- js/seeker.js
+++ js/seeker.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('#submit').click(function(e){
+  $('#submit_button').click(function(e){
     window.location.replace("http://www.mit.edu/~uayyaz/bloodshare/GR4/results.html");
   })
 });
